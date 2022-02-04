c1ff869475660b73dc50cc7aeb5133e287f83023
Shawn Kebel <fxrender@outlook.com>
2022-02-04 16:14:56 -0600

    :necktie: Add `pages` component routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import './App.css';
+import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import Register from './pages/Register.js';
+import BookingCar from './pages/BookingCar.js';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+        {/* <h1>RIDE ON THE SIDE</h1> */}
+            <BrowserRouter>
+
+                <Route path='/' exact component={Home} />
+                <Route path='/login' exact component={Login} />
+                <Route path='/register' exact component={Register} />
+                <Route path='/bookingcar' exact component={BookingCar} />
+
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
