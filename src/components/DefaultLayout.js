a786ef2b62b7150353c8b2cf7bcd237df17d9947
Shawn Kebel <fxrender@outlook.com>
2022-02-04 18:00:15 -0600

    :lipstick: `DefaultLayout`

--- src/components/DefaultLayout.js
+++ src/components/DefaultLayout.js
@@ -3,16 +3,16 @@ import React from 'react';
 function DefaultLayout(props) {
   return (
     <div>
-      <div className="header">
+      <div className="header bs1">
         <div className="d-flex justify-content-between">
+
           <h1>Ride On The Side</h1>
-          <button>user</button>
+
         </div>
       </div>
       <div className="content">
         {props.children}
       </div>
-
     </div>
   );
 }
