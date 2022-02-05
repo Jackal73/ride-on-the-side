2f9d20e0bf324a48ef5f926b6acd43b50bf1c531
Shawn Kebel <fxrender@outlook.com>
2022-02-05 15:17:57 -0600

    :zap: Add redux `carsReducer`

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import DefaultLayout from '../components/DefaultLayout';
 
 function Home() {
+  const {cars} = useSelector(state=>state.carsReducer)
   return (
     <DefaultLayout>
         <h1>Home page</h1>
+        <h1>The length of the cars array is {cars.length}</h1>
     </DefaultLayout>
     );
   }
