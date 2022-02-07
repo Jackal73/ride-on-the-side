d1f916ab2169f1448fa2a2806a030a32fc7689bb
Shawn Kebel <fxrender@outlook.com>
2022-02-07 12:00:35 -0600

    :zap: Add redux `userActions`

---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { message } from 'antd'
+
+export const userLogin = (reqObj) => async dispatch => {
+
+  dispatch({
+    type: 'LOADING',
+    payload: true
+  })
+
+  try {
+    const response = await axios.post('/api/users/login', reqObj)
+    localStorage.setItem('user', JSON.stringify(response.data))
+    message.success('Login was successful!')
+    dispatch({
+      type: 'LOADING',
+      payload: false
+    })
+  } catch (error) {
+    console.log(error)
+    message.error('Something went wrong...')
+    dispatch({
+      type: 'LOADING',
+      payload: false
+    })
+  }
+}
+
+export const userRegister = (reqObj) => async dispatch => {
+
+  dispatch({
+    type: 'LOADING',
+    payload: true
+  })
+
+  try {
+    const response = await axios.post('/api/users/login', reqObj)
+
+    dispatch({
+      type: 'LOADING',
+      payload: false
+    })
+    message.success('Registration was successful!')
+  } catch (error) {
+    console.log(error)
+    message.error('Something went wrong...')
+    dispatch({
+      type: 'LOADING',
+      payload: false
+    })
+  }
+}
