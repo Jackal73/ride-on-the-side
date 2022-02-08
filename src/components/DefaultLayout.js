9a26f531eac03a44ad75dc0201341fa7318fdeff
Shawn Kebel <fxrender@outlook.com>
2022-02-08 01:13:49 -0600

    :boom: Add dropdown `Menu` to 'defaultLayout'

--- src/components/DefaultLayout.js
+++ src/components/DefaultLayout.js
@@ -1,14 +1,47 @@
 import React from 'react';
+import { Button, Dropdown, Menu, Col, Row } from 'antd';
+
 
 function DefaultLayout(props) {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const menu = (
+    <Menu>
+    <Menu.Item>
+        <a href="https://www.antgroup.com">
+          Home
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="https://www.antgroup.com">
+          Bookings
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="https://www.aliyun.com">
+          Profile
+        </a>
+      </Menu.Item>
+      <Menu.Item onClick={() => {
+        localStorage.removeItem('user');
+        window.location.href='/login';
+      }}>
+        <li>Logout</li>
+      </Menu.Item>
+    </Menu>
+  );
   return (
     <div>
       <div className="header bs1">
-        <div className="d-flex justify-content-between">
-
-          <h1>Ride On The Side</h1>
-
-        </div>
+        <Row gutter={16} justify="center">
+          <Col lg={20} sm={24} xs={24}>
+            <div className="d-flex justify-content-between">
+              <h1>Ride On The Side</h1>
+              <Dropdown overlay={menu} placement="bottomCenter">
+                <Button>{user.username}</Button>
+              </Dropdown>
+            </div>
+          </Col>
+        </Row>
       </div>
       <div className="content">
         {props.children}
