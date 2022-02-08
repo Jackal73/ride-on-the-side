45f6d0237c8a97284bb04c283f705dc5a98ee505
Shawn Kebel <fxrender@outlook.com>
2022-02-08 01:34:17 -0600

    :dizzy: Add animation to `Login/Register` pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,18 @@
 import { Col, Form, Input, Row } from 'antd';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux'
 import { userLogin } from '../redux/actions/userActions';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Spinner from '../components/Spinner';
 
 
+AOS.init();
 
 function Login() {
   const dispatch = useDispatch()
+  const {loading} = useSelector(state => state.alertsReducer)
   function onFinish(values) {
     dispatch(userLogin(values))
     console.log(values);
@@ -15,12 +20,14 @@ function Login() {
 
   return (
     <div className="login">
+      {loading && (<Spinner />)}
 
       <Row gutter={16} className="d-flex align-items-center">
 
         <Col lg={16} style={{position: 'relative'}}>
+
+          <img data-aos='slide-right' data-aos-duration='1500' src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" alt="" />
           <h1 className="login-logo">Ride On The Side</h1>
-          <img src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" alt="" />
         </Col>
         <Col lg={8} className="text-left p-5">
           <Form layout='vertical' className="login-form p-5" onFinish={onFinish}>
