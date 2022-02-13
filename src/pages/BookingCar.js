e16c67836019fe0a3624fdf144a64a6f60c98d64
Shawn Kebel <fxrender@outlook.com>
2022-02-12 20:48:06 -0600

    :construction::sparkles: Work on booking `Time Slots`

--- src/pages/BookingCar.js
+++ src/pages/BookingCar.js
@@ -2,24 +2,40 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import DefaultLayout from '../components/DefaultLayout';
 import Spinner from '../components/Spinner';
-import { Row, Col, Divider } from 'antd';
+import { Row, Col, Divider, DatePicker } from 'antd';
 import { getAllCars } from '../redux/actions/carsActions';
+import moment from 'moment';
+
+const { RangePicker } = DatePicker;
 
 function BookingCar({match}) {
   const {cars} = useSelector(state => state.carsReducer)
   const {loading} = useSelector(state => state.alertsReducer)
   const [car, setcar] = useState({})
   const dispatch = useDispatch()
+  const[from, setFrom] = useState()
+  const [to, setTo] = useState()
+  const [totalHours, setTotalHours] = useState(0)
 
   useEffect(() => {
-    
-    if (cars.length === 0) { 
-      dispatch(getAllCars());      
+
+    if (cars.length === 0) {
+      dispatch(getAllCars());
     } else {
       setcar(cars.find(o => o._id === match.params.carid))
     }
-    
+
   }, [cars]);
+
+  function selectTimeSlots(values) {
+    console.log(moment(values[0]).format('MMM DD YYYY h:00 a'));
+    console.log(moment(values[1]).format('MMM DD YYYY h:00 a'));
+    setFrom(moment(values[0]).format('MMM DD YYYY h:00 a'))
+    setTo(moment(values[1]).format('MMM DD YYYY h:00 a'))
+
+    setTotalHours(values[1].diff(values[0], 'Hours'))
+
+  }
 
   return (
   <DefaultLayout>
@@ -30,14 +46,23 @@ function BookingCar({match}) {
         <img src={car.image} className="carimg2 bs1" alt="" />
       </Col>
 
-      <Col lg={10} sm={24} xs={24} className='text-right'>        
-        <Divider type='horizontal' dashed>Car Info</Divider>
+      <Col lg={10} sm={24} xs={24} className='text-right'>
+        <Divider type='horizontal' dashed style={{fontWeight: 'semi-bold'}} >Car Info</Divider>
         <div className="" style={{ textAlign: 'right' }} >
           <p>{car.name}</p>
           <p>{car.rentPerHour} Rent Per Hour</p>
           <p>Fuel Type : {car.fuelType}</p>
           <p>Max Persons : {car.capacity}</p>
         </div>
+
+        <Divider type='horizontal' dashed style={{fontWeight: 'semi-bold'}} >Select Time Slots</Divider>
+        {/* <RangePicker format='MMM DD YYYY h:00 a' onChange={selectTimeSlots} /> */}
+        <RangePicker showTime={{format: 'h:00 a'}} format='MMM DD YYYY h:00 a' onChange={selectTimeSlots} />
+
+        <div className="">
+          <p>Total Hours : {totalHours}</p>
+        </div>
+
       </Col>
 
     </Row>
