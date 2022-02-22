27544f705a59ee21bbe9faf66cb63665c23357b5
Shawn Kebel <fxrender@outlook.com>
2022-02-22 03:27:25 -0600

    :necktie: Change Logo name: `DRIVE_STYLe`

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,43 +1,57 @@
-import { Col, Form, Input, Row } from 'antd';
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { userLogin } from '../redux/actions/userActions';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import Spinner from '../components/Spinner';
-
+import { Col, Form, Input, Row } from "antd";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { userLogin } from "../redux/actions/userActions";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Spinner from "../components/Spinner";
 
 AOS.init();
 
 function Login() {
-  const dispatch = useDispatch()
-  const {loading} = useSelector(state => state.alertsReducer)
+  const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.alertsReducer);
   function onFinish(values) {
-    dispatch(userLogin(values))
+    dispatch(userLogin(values));
     console.log(values);
   }
 
   return (
     <div className="login">
-      {loading && (<Spinner />)}
+      {loading && <Spinner />}
 
       <Row gutter={16} className="d-flex align-items-center">
-
-        <Col lg={16} style={{position: 'relative'}}>
-
-          <img data-aos='slide-right' data-aos-duration='1500' src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" alt="" />
-          <h1 className="login-logo">Ride On The Side</h1>
+        <Col lg={16} style={{ position: "relative" }}>
+          <img
+            data-aos="slide-right"
+            data-aos-duration="1500"
+            src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+            alt=""
+          />
+          <h1 className="login-logo">DRIVE_STYLe</h1>
         </Col>
         <Col lg={8} className="text-left p-5">
-          <Form layout='vertical' className="login-form p-5" onFinish={onFinish}>
+          <Form
+            layout="vertical"
+            className="login-form p-5"
+            onFinish={onFinish}
+          >
             <h1>Login</h1>
             <hr />
 
-            <Form.Item name='username' label='Username' rules={[{required: true}]}>
+            <Form.Item
+              name="username"
+              label="Username"
+              rules={[{ required: true }]}
+            >
               <Input />
             </Form.Item>
-            <Form.Item name='password' label='Password' rules={[{required: true}]}>
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[{ required: true }]}
+            >
               <Input />
             </Form.Item>
 
@@ -45,14 +59,10 @@ function Login() {
 
             <hr />
 
-            <Link to='/register'>Click Here to Register</Link>
-
+            <Link to="/register">Click Here to Register</Link>
           </Form>
         </Col>
-
-
       </Row>
-
     </div>
   );
 }
