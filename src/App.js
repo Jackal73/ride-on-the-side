b4b87cd28aee0d961133ded6b5fe95ef841907cf
Shawn Kebel <fxrender@outlook.com>
2022-02-23 22:24:07 -0600

    :sparkles: Add `new car` page - design

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
-import 'antd/dist/antd.css';
-import React from 'react';
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
-import './App.css';
-import BookingCar from './pages/BookingCar.js';
-import Home from './pages/Home.js';
-import Login from './pages/Login.js';
-import Register from './pages/Register.js';
-import UserBookings from './pages/UserBookings';
-
-
-
+import "antd/dist/antd.css";
+import React from "react";
+import { BrowserRouter, Redirect, Route } from "react-router-dom";
+import "./App.css";
+import AddCar from "./pages/AddCar";
+import BookingCar from "./pages/BookingCar.js";
+import Home from "./pages/Home.js";
+import Login from "./pages/Login.js";
+import Register from "./pages/Register.js";
+import UserBookings from "./pages/UserBookings";
 
 function App() {
-    return (
-        <div className="App">
-        {/* <h1>RIDE ON THE SIDE</h1> */}
-            <BrowserRouter>
-
-                <ProtectedRoute path='/' exact component={Home} />
-                <Route path='/login' exact component={Login} />
-                <Route path='/register' exact component={Register} />
-                <ProtectedRoute path='/booking/:carid' exact component={BookingCar} />
-                <ProtectedRoute path='/userbookings' exact component={UserBookings} />
-
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      {/* <h1>RIDE ON THE SIDE</h1> */}
+      <BrowserRouter>
+        <ProtectedRoute path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <ProtectedRoute path="/booking/:carid" exact component={BookingCar} />
+        <ProtectedRoute path="/userbookings" exact component={UserBookings} />
+        <ProtectedRoute path="/addcar" exact component={AddCar} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
 
-export function ProtectedRoute(props)
-{
-
-
-    if(localStorage.getItem('user'))
-    {
-        return <Route {...props} />;
-    }
-    else {
-        return <Redirect to='/login' />
-    }
+export function ProtectedRoute(props) {
+  if (localStorage.getItem("user")) {
+    return <Route {...props} />;
+  } else {
+    return <Redirect to="/login" />;
+  }
 }
--- src/redux/actions/carsActions.js
+++ src/redux/actions/carsActions.js
@@ -1,27 +1,53 @@
-import axios from 'axios'
+import { message } from "antd";
+import axios from "axios";
 
-export const getAllCars = () => async dispatch => {
-
+export const getAllCars = () => async (dispatch) => {
   dispatch({
-    type: 'LOADING',
-    payload: true
-  })
+    type: "LOADING",
+    payload: true,
+  });
 
   try {
-    const response = await axios.get('/api/cars/getallcars')
+    const response = await axios.get("/api/cars/getallcars");
     dispatch({
-      type: 'GET_ALL_CARS',
-      payload: response.data
-    })
+      type: "GET_ALL_CARS",
+      payload: response.data,
+    });
     dispatch({
-      type: 'LOADING',
-      payload: false
-    })
+      type: "LOADING",
+      payload: false,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     dispatch({
-      type: 'LOADING',
-      payload: false
-    })
+      type: "LOADING",
+      payload: false,
+    });
   }
-}
+};
+
+export const addCars = (reqObj) => async (dispatch) => {
+  dispatch({
+    type: "LOADING",
+    payload: true,
+  });
+
+  try {
+    await axios.post("/api/cars/addcar", reqObj);
+
+    dispatch({
+      type: "LOADING",
+      payload: false,
+    });
+    message.success("New Car added successfully");
+    setTimeout(() => {
+      window.location.href = "/";
+    }, 1000);
+  } catch (error) {
+    console.log(error);
+    dispatch({
+      type: "LOADING",
+      payload: false,
+    });
+  }
+};
