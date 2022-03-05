4fd8095b2183052d91592535bf505dd22fc4f5c1
Shawn Kebel <fxrender@outlook.com>
2022-03-04 19:15:53 -0600

    :necktie: Add  `addAllBookings` to `bookingActions`

--- src/redux/actions/bookingActions.js
+++ src/redux/actions/bookingActions.js
@@ -1,27 +1,42 @@
-import axios from 'axios';
-import { message } from 'antd';
+import { message } from "antd";
+import axios from "axios";
 
 export const bookCar = (reqObj) => async (dispatch) => {
-
   dispatch({
-    type: 'LOADING',
-    payload: true
+    type: "LOADING",
+    payload: true,
   });
 
   try {
-    await axios.post('/api/bookings/bookcar', reqObj);
+    await axios.post("/api/bookings/bookcar", reqObj);
 
     dispatch({
-      type: 'LOADING',
-      payload: false
+      type: "LOADING",
+      payload: false,
     });
-    message.success('Your car and time slot have been booked!');
+    message.success("Your car and time slot have been booked!");
+    setTimeout(() => {
+      window.location.href = "/userbookings";
+    }, 500);
   } catch (error) {
     console.log(error);
     dispatch({
-      type: 'LOADING',
-      payload: false
+      type: "LOADING",
+      payload: false,
     });
-    message.error('Something went wrong. Please try again later');
+    message.error("Something went wrong. Please try again later");
   }
-}
+};
+
+export const getAllBookings = () => async (dispatch) => {
+  dispatch({ type: "LOADING", payload: true });
+
+  try {
+    const response = await axios.get("/api/bookings/getallbookings");
+    dispatch({ type: "GET_ALL_BOOKINGS", payload: response.data });
+    dispatch({ type: "LOADING", payload: false });
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: "LOADING", payload: false });
+  }
+};
